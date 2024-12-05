189f958adee018320a45f4cc46e13ac9dec81852
xgvx <grischa.vinogradov@gmail.com>
2024-12-05 13:34:57 +0100

    daten

---
+++ ausbildungsnachweise.js
@@ -0,0 +1,13 @@
+class weekday {
+    constructor(name) {
+        this.name = name;
+        this.activities = [];
+    }
+}
+
+class activity {
+    constructor(name, hours) {
+        this.name = name;
+        this.hours = hours;
+    }
+}
