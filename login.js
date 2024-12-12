3c627e44293220931733a1ebc60b34d37973ff97
xgvx <grischa.vinogradov@gmail.com>
2024-12-12 12:31:04 +0100

    viele lächerliche methoden

--- login.js
+++ login.js
@@ -1,14 +1,41 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-    const message = document.getElementById('message');
+class login {
+    message = document.getElementById('message');
 
-    if (username === 'admin' && password === 'password') {
-        message.textContent = 'Login erfolgreich!';
-        message.style.color = 'green';
-        window.location.href = "./index.html";
-    } else {
+    handleSubmit = function () {
+        this.checkLogin();
+    }
+
+    checkLogin() {
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        if (username === 'admin' && password === 'password') {
+            this.displaySuccessMessage()
+            this.redirect()
+        } else {
+            this.displayErrorMessage()
+        }
+    }
+
+    displayErrorMessage() {
         message.textContent = 'Ungültiger Benutzername oder Passwort';
     }
-});
+
+    displaySuccessMessage() {
+        const message = document.getElementById('message');
+        
+        message.textContent = 'Login erfolgreich!';
+        message.style.color = 'green';
+    }
+
+    redirect() {
+        window.location.href = "./index.html";
+    }
+    
+}
+
+login = new login()
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    login.handleSubmit();
+})
