bd2cdb55573ca37d0f961fabb095f8e1f1828cc4
Aaron Selbach <aaron.selbach@gus-group.com>
2025-01-09 14:16:30 +0100

    js stuff

---
+++ berichtsheft.js
@@ -0,0 +1,6 @@
+function addActivity(){
+    console.log("dsfdd")
+    let child = document.createElement("div");
+    child.innerText=document.getElementById("MondayActivity").value;
+    document.getElementById("Monday").appendChild(child);
+}
